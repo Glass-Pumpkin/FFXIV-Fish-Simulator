c643ce6333add8b0da82863156e9241c32d84b1c
Dylan <87920137+Glass-Pumpkin@users.noreply.github.com>
2022-07-06 02:53:11 -0400

    v2.0

--- game.js
+++ game.js
@@ -1,413 +1,4 @@
-let fishInfo = [
-    {
-        name: 'Merlthor Goby',
-        level: 3,
-        bait: ['Lugworm', 'Versatile Lure'],
-        bite: '!',
-        time: 5,
-        mooch: 'Yes',
-        minSize: 3.7,
-        maxSize: 9.3,
-        image: 'FishImg/Merlthor Goby.png'
-    },
-
-    {
-        name: 'Finger Shrimp',
-        level: 4,
-        bait: ['Lugworm', 'Versatile Lure'],
-        bite: '!',
-        time: 5,
-        mooch: 'Yes',
-        minSize: 4.3,
-        maxSize: 10.1,
-        image: 'FishImg/Finger Shrimp.png'
-    },
-
-    {
-        name: 'Ocean Cloud',
-        level: 6,
-        bait: ['Pill Bug', 'Versatile Lure'],
-        bite: '!',
-        time: 5,
-        mooch: 'Yes',
-        minSize: 4.9,
-        maxSize: 10.5,
-        image: 'FishImg/Ocean Cloud.png'
-    },
-
-    {
-        name: 'Harbor Herring',
-        level: 7,
-        bait: ['Pill Bug', 'Versatile Lure'],
-        bite: '!',
-        time: 5,
-        mooch: 'Yes',
-        minSize: 5.8,
-        maxSize: 12.8,
-        image: 'FishImg/Harbor Herring.png'
-    },
-
-    {
-        name: 'Malm Kelp',
-        level: 4,
-        bait: ['Lugworm', 'Versatile Lure'],
-        bite: '!',
-        time: 5,
-        mooch: 'No',
-        minSize: 6.2,
-        maxSize: 8.6,
-        image: 'FishImg/Malm Kelp.png'
-    },
-
-    {
-        name: 'Coral Butterfly',
-        level: 9,
-        bait: ['Pill Bug', 'Versatile Lure'],
-        bite: '!!',
-        time: 8,
-        mooch: 'No',
-        minSize: 5.6,
-        maxSize: 13.2,
-        image: 'FishImg/Coral Butterfly.png'
-    },
-
-    {
-        name: 'Sea Cucumber',
-        level: 7,
-        bait: ['Pill Bug', 'Versatile Lure'],
-        bite: '!',
-        time: 5,
-        mooch: 'No',
-        minSize: 7.1,
-        maxSize: 9,
-        image: 'FishImg/Sea Cucumber.png'
-    },
-
-    {
-        name: 'Angelfish',
-        level: 19,
-        bait: ['Rat Tail', 'Versatile Lure'],
-        bite: '!!',
-        time: 8,
-        mooch: 'No',
-        minSize: 21.7,
-        maxSize: 30.2,
-        image: 'FishImg/Angelfish.png'
-    },
-
-    {
-        name: 'Blowfish',
-        level: 22,
-        bait: ['Spoon Worm', 'Versatile Lure'],
-        bite: '!!',
-        time: 8,
-        mooch: 'No',
-        minSize: 9.3,
-        maxSize: 10.2,
-        image: 'FishImg/Blowfish.png'
-    },
-
-    {
-        name: 'Leafy Seadragon',
-        level: 33,
-        bait: ['Spoon Worm', 'Versatile Lure'],
-        bite: '!',
-        time: 5,
-        mooch: 'No',
-        minSize: 17.6,
-        maxSize: 34.4,
-        image: 'FishImg/Leafy Seadragon.png'
-    },
-
-    {
-        name: 'Wahoo',
-        level: 43,
-        bait: '', //Merlthor Goby
-        bite: '!!',
-        time: 8,
-        mooch: 'No',
-        minSize: 24.7,
-        maxSize: 60.9,
-        image: 'FishImg/Wahoo.png'
-    },
-
-    {
-        name: 'Red Coral',
-        level: 40,
-        bait: ['Versatile Lure'],
-        bite: '!',
-        time: 5,
-        mooch: 'No',
-        minSize: 14.2,
-        maxSize: 34,
-        image: 'FishImg/Red Coral.png'
-    },
-
-    {
-        name: 'Balloonfish',
-        level: 37,
-        bait: ['Versatile Lure'],
-        bite: '!!',
-        time: 8,
-        mooch: 'No',
-        minSize: 20.7,
-        maxSize: 47.4,
-        image: 'FishImg/Balloonfish.png'
-    },
-
-    {
-        name: 'Rothlyt Oyster',
-        level: 17,
-        bait: ['Rat Tail', 'Versatile Lure'],
-        bite: '!',
-        time: 5,
-        mooch: 'No',
-        minSize: 4.1,
-        maxSize: 9.4,
-        image: 'FishImg/Rothlyt Oyster.png'
-    },
-
-    {
-        name: 'Kissing Fish',
-        level: 54,
-        bait: ['Brute Leech', 'Versatile Lure'],
-        bite: '!',
-        time: 5,
-        mooch: 'No',
-        minSize: 19.8,
-        maxSize: 25.4,
-        image: 'FishImg/Kissing Fish.png'
-    },
-
-    {
-        name: 'Dravanian Squeaker',
-        level: 54,
-        bait: ['Brute Leech', 'Versatile Lure'],
-        bite: '!',
-        time: 5,
-        mooch: 'No',
-        minSize: 13.4,
-        maxSize: 18,
-        image: 'FishImg/Dravanian Squeaker.png'
-    },
-
-    {
-        name: 'Gnomefish',
-        level: 60,
-        bait: ['Brute Leech', 'Versatile Lure'],
-        bite: '!',
-        time: 5,
-        mooch: 'No',
-        minSize: 5.2,
-        maxSize: 8.9,
-        image: 'FishImg/Gnomefish.png'
-    },
-
-    {
-        name: 'Blackfin Snake Eel',
-        level: 70,
-        bait: ['Versatile Lure'],
-        bite: '!!',
-        time: 8,
-        mooch: 'No',
-        minSize: 28.4,
-        maxSize: 38.8,
-        image: 'FishImg/Blackfin Snake Eel.png'
-    },
-
-    {
-        name: 'Glass Flounder',
-        level: 63,
-        bait: ['Versatile Lure'],
-        bite: '!!',
-        time: 8,
-        mooch: 'No',
-        minSize: 11.5,
-        maxSize: 13.6,
-        image: 'FishImg/Glass Flounder.png'
-    },
-
-    {
-        name: 'Coeurl Snake Eel',
-        level: 63,
-        bait: ['Versatile Lure'],
-        bite: '!!',
-        time: 8,
-        mooch: 'No',
-        minSize: 35.6,
-        maxSize: 39.2,
-        image: 'FishImg/Coeurl Snake Eel.png'
-    },
-
-    {
-        name: 'Copperfish',
-        level: 20,
-        bait: ['Versatile Lure'],
-        bite: '!',
-        time: 5,
-        mooch: 'Yes',
-        minSize: 6.7,
-        maxSize: 10.9,
-        image: 'FishImg/Copperfish.png'
-    },
-
-    {
-        name: 'Monke Onke',
-        level: 33,
-        bait: '', //Mooch Cooperfish
-        bite: '!',
-        time: 5,
-        mooch: 'No',
-        minSize: 22.1,
-        maxSize: 36.9,
-        image: 'FishImg/Monke Onke.png'
-    },
-
-    {
-        name: 'Rock Lobster',
-        level: 50,
-        bait: ['Versatile Lure'],
-        bite: '!!',
-        time: 8,
-        mooch: 'No',
-        minSize: 22.5,
-        maxSize: 31.3,
-        image: 'FishImg/Rock Lobster.png'
-    },
-
-    {
-        name: 'Bullfrog',
-        level: 55,
-        bait: ['Brute Leech', 'Versatile Lure'],
-        bite: '!',
-        time: 5,
-        mooch: 'No',
-        minSize: 5.5,
-        maxSize: 12.5,
-        image: 'FishImg/Bullfrog.png'
-    },
-
-    {
-        name: 'Spotted Puffer',
-        level: 38,
-        bait: ['Versatile Lure'],
-        bite: '!',
-        time: 5,
-        mooch: 'No',
-        minSize: 3.9,
-        maxSize: 6.7,
-        image: 'FishImg/Spotted Puffer.png'
-    },
-
-    {
-        name: 'Monkfish',
-        level: 26,
-        bait: ['Spoonworm', 'Versatile Lure'],
-        bite: '!!',
-        time: 8,
-        mooch: 'No',
-        minSize: 23.9,
-        maxSize: 58.5,
-        image: 'FishImg/Monkfish.png'
-    },
-
-    {
-        name: 'Fullmoon Sardine',
-        level: 34,
-        bait: ['Spoon Worm', 'Versatile Lure'],
-        bite: '!',
-        time: 5,
-        mooch: 'Yes',
-        minSize: 9.8,
-        maxSize: 21.5,
-        image: 'FishImg/Fullmoon Sardine.png'
-    },
-
-    {
-        name: 'Bianaq Bream',
-        level: 27,
-        bait: ['Spoon Worm', 'Versatile Lure'],
-        bite: '!!',
-        time: 8,
-        mooch: 'No',
-        minSize: 21.8,
-        maxSize: 51,
-        image: 'FishImg/Bianaq Bream.png'
-    },
-
-    {
-        name: 'Indigo Herring',
-        level: 31,
-        bait: ['Spoon Worm', 'Versatile Lure'],
-        bite: '!!',
-        time: 8,
-        mooch: 'No',
-        minSize: 24.5,
-        maxSize: 42.9,
-        image: 'FishImg/Indigo Herring.png'
-    },
-
-    {
-        name: 'Lavender Remora',
-        level: 36,
-        bait: ['Versatile Lure'],
-        bite: '!!',
-        time: 8,
-        mooch: 'No',
-        minSize: 29.9,
-        maxSize: 59.9,
-        image: 'FishImg/Lavender Remora.png'
-    },
-
-    {
-        name: 'Ash Tuna',
-        level: 32,
-        bait: ['Spoon Worm', 'Versatile Lure'],
-        bite: '!!',
-        time: 8,
-        mooch: 'No',
-        minSize: 30.4,
-        maxSize: 69.1,
-        image: 'FishImg/Ash Tuna.png'
-    },
-
-    {
-        name: 'Shark Tuna',
-        level: 50,
-        bait: '', //Fullmoon Sardine
-        bite: '!!!',
-        time: 15,
-        mooch: 'No',
-        minSize: 89.2,
-        maxSize: 102.7,
-        image: 'FishImg/Shark Tuna.png'
-    },
-
-    {
-        name: 'Silver Shark',
-        level: 41,
-        bait: '', //Merlthor Goby
-        bite: '!!!',
-        time: 15,
-        mooch: 'No',
-        minSize: 33.1,
-        maxSize: 82.5,
-        image: 'FishImg/Silver Shark.png'
-    },
-
-    {
-        name: 'Gigantshark',
-        level: 50,
-        bait: '', //Merlthor Goby
-        bite: '!!!',
-        time: 15,
-        mooch: 'No',
-        minSize: 94.7,
-        maxSize: 123.9,
-        image: 'FishImg/Gigantshark.png'
-    },
-];
-
+let fishInfo = coerthasFish.concat(thanalanFish, blackShroudFish, laNosceaFish, farEastFish);
 
 var caught = false;
 var hasBite = false;
@@ -426,6 +17,35 @@ let timeTwo;
 // let iteration;
 // let castMessage;
 
+function teleport() {
+    document.body.style.background =
+        'linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url(Misc/Fish.jpg) no-repeat';
+}
+
+function limsa() {
+    document.body.style.background =
+        'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(Misc/Limsa.jpg) no-repeat';
+}
+
+function gridania() {
+    document.body.style.background =
+        'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(Misc/Gridania.jpg) no-repeat';
+}
+
+function uldah() {
+    document.body.style.background =
+        'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(Misc/Uldah.jpg) no-repeat';
+}
+
+function ishgard() {
+    document.body.style.background =
+        'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(Misc/Ishgard.jpg) no-repeat';
+}
+
+function kugane() {
+    document.body.style.background =
+        'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(Misc/Kugane.jpg) no-repeat';
+}
 
 function chumMath(x) {
     return x - (x * .4);
@@ -468,6 +88,17 @@ function resetPage() {
     caught = false;
 }
 
+function selectClean(x) {
+    chum.className = "";
+    laNoscea.className = "";
+    blackShroud.className = "";
+    thanalan.className = "";
+    coerthas.className = "";
+    farEast.className = "";
+    home.className = ""
+
+    x.className = "select";
+}
 
 castOut.addEventListener("click", function (e) {
 
@@ -545,7 +176,65 @@ quit.addEventListener("click", function (e) {
     outputOne.innerHTML = `You put away your rod.`
 });
 
+home.addEventListener("click", function (e) {
+    fishInfo = coerthasFish.concat(thanalanFish, blackShroudFish, laNosceaFish, farEastFish);
+    teleport();
+    resetPage();
+    clearTimeout(timeTwo);
+    clearTimeout(fishTimer);
+    selectClean(home);
+    outputOne.innerHTML = `You teleport back home.`
+});
 
+laNoscea.addEventListener("click", function (e) {
+    fishInfo = laNosceaFish;
+    limsa();
+    resetPage();
+    clearTimeout(timeTwo);
+    clearTimeout(fishTimer);
+    selectClean(laNoscea);
+    outputOne.innerHTML = `You teleport to La Noscea.`
+});
+
+blackShroud.addEventListener("click", function (e) {
+    fishInfo = blackShroudFish;
+    gridania();
+    resetPage();
+    clearTimeout(timeTwo);
+    clearTimeout(fishTimer);
+    selectClean(blackShroud);
+    outputOne.innerHTML = `You teleport to The Black Shroud.`
+});
+
+thanalan.addEventListener("click", function (e) {
+    fishInfo = thanalanFish;
+    uldah();
+    resetPage();
+    clearTimeout(timeTwo);
+    clearTimeout(fishTimer);
+    selectClean(thanalan);
+    outputOne.innerHTML = `You teleport to Thanalan.`
+});
+
+coerthas.addEventListener("click", function (e) {
+    fishInfo = coerthasFish;
+    ishgard();
+    resetPage();
+    clearTimeout(timeTwo);
+    clearTimeout(fishTimer);
+    selectClean(coerthas);
+    outputOne.innerHTML = `You teleport to Coerthas.`
+});
+
+farEast.addEventListener("click", function (e) {
+    fishInfo = farEastFish;
+    kugane();
+    resetPage();
+    clearTimeout(timeTwo);
+    clearTimeout(fishTimer);
+    selectClean(farEast)
+    outputOne.innerHTML = `You teleport to The Far East.`
+});
 
 
 
